6eda26b0509d2cb633e91ba20dc6fd7736399780
drJanebug <rainbowbug98@gmail.com>
2025-12-18 14:12:17 +0500

    папки клиента

---
+++ client/src/shared/lib/axiosInstance.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:3000",
+  withCredentials: true,
+});
+
+let accessToken = "";
+
+function setAccessToken(newToken) {
+  accessToken = newToken;
+}
+
+// отправляем запрос и проверяем наличие заголовков авторизации и токена в загловке
+axiosInstance.interceptors.request.use((config) => {
+  if (!config.headers.Authorization) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const prevRequest = error.config;
+    if (error.response.status === 403 && !prevRequest.sent) {
+      const response = await axiosInstance("/api/auth/refreshTokens");
+      accessToken = response.data.data.accessToken;
+      prevRequest.sent = true;
+      prevRequest.headers.Authorization = `Bearer ${accessToken}`;
+      return axiosInstance(prevRequest);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { setAccessToken };
+
+
+export default axiosInstance;
